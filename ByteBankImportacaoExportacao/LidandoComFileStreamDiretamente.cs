bb8dbe0c05a7b5f84a94a6ca94b9a3cc13cb5c0d
Rodolfo José <rodolfo.santos@exemplo.com>
2019-06-19 17:37:55 -0300

    at work quarta

---
+++ ByteBankImportacaoExportacao/LidandoComFileStreamDiretamente.cs
@@ -0,0 +1,46 @@
+﻿using ByteBankImportacaoExportacao.Modelos;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBankImportacaoExportacao
+{
+    partial class Program
+    {
+        static void FileStreamDiretamente()
+        {
+            var enderecoArquivo = "contas.txt";
+            using (var fluxoDoArquivo = new FileStream(enderecoArquivo, FileMode.Open))
+            {
+                var buffer = new byte[1024]; //1kb
+                var numeroBytesLidos = -1;
+
+                while (numeroBytesLidos != 0)
+                {
+                    numeroBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024);
+                    Console.WriteLine($" Bytes lidos: {numeroBytesLidos}");
+                    EscreverBuffer(buffer, numeroBytesLidos);
+                }
+            }
+        }
+        static void EscreverBuffer(byte[] buffer, int bytesLidos)
+        {
+            //var utf8 = new UTF8Encoding();
+            //var utf8 = Encoding.UTF8;
+            var utf8 = Encoding.Default;
+
+            var texto = utf8.GetString(buffer, 0, bytesLidos);
+
+            Console.Write(texto);
+
+            //foreach (var meuByte in buffer)
+            //{
+            //    Console.Write(meuByte);
+            //    Console.Write(" ");
+            //}
+        }
+    }
+}
--- ByteBankImportacaoExportacao/Program.cs
+++ ByteBankImportacaoExportacao/Program.cs
@@ -6,37 +6,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ByteBankImportacaoExportacao 
-{ 
-    class Program 
-    { 
-        static void Main(string[] args) 
+namespace ByteBankImportacaoExportacao
+{
+    partial class Program
+    {
+        static void Main(string[] args)
         {
-            //var enderecoArquivo = "c:/projetoAlura/contas.txt";
-            var enderecoArquivo = "contas.txt"; // só acha o txt pois está na pasta debug a mesma do .exe
-
-            var fluxoDoArquivo = new FileStream(enderecoArquivo, FileMode.Open);
-            var buffer = new byte[1024]; //1kb
-
-            var numeroBytesLidos = -1;
-
-            while (numeroBytesLidos != 0)
+            var enderecoArquivo = "contas.txt";
+            using (var fluxoDeArquivo = new FileStream(enderecoArquivo, FileMode.Open))
             {
-                numeroBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024);
-                EscreverBuffer(buffer);
+                var leitor = new StreamReader(fluxoDeArquivo);
+                var linha = leitor.ReadToEnd();
+                Console.WriteLine(linha);
             }
 
             Console.ReadLine();
         }
 
-        static void EscreverBuffer(byte[] buffer)
-        {
-            foreach (var meuByte in buffer)
-            {
-                Console.Write(meuByte);
-                Console.Write(" ");
-            }
-        }
+       
     }
-} 
- 
+}
