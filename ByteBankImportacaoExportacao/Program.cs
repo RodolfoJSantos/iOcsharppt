4205dffcf1d664cb3714422b8e24a294f0320a67
rodolfo santos <rodjsant@outlook.com>
2019-06-18 22:33:26 -0300

    commit inicial

---
+++ ByteBankImportacaoExportacao/Program.cs
@@ -0,0 +1,42 @@
+﻿using ByteBankImportacaoExportacao.Modelos;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBankImportacaoExportacao 
+{ 
+    class Program 
+    { 
+        static void Main(string[] args) 
+        {
+            //var enderecoArquivo = "c:/projetoAlura/contas.txt";
+            var enderecoArquivo = "contas.txt"; // só acha o txt pois está na pasta debug a mesma do .exe
+
+            var fluxoDoArquivo = new FileStream(enderecoArquivo, FileMode.Open);
+            var buffer = new byte[1024]; //1kb
+
+            var numeroBytesLidos = -1;
+
+            while (numeroBytesLidos != 0)
+            {
+                numeroBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024);
+                EscreverBuffer(buffer);
+            }
+
+            Console.ReadLine();
+        }
+
+        static void EscreverBuffer(byte[] buffer)
+        {
+            foreach (var meuByte in buffer)
+            {
+                Console.Write(meuByte);
+                Console.Write(" ");
+            }
+        }
+    }
+} 
+ 
