42b9e0e606dc5e6d67d6ce04c709f03c39afd395
Rodolfo José <rodolfo.santos@exemplo.com>
2019-06-25 14:19:18 -0300

    atualização de acordo com aula

--- ByteBankImportacaoExportacao/CriandoArquivo.cs
+++ ByteBankImportacaoExportacao/CriandoArquivo.cs
@@ -11,6 +11,26 @@ namespace ByteBankImportacaoExportacao
 
             var caminhoNovoArquivo = "contasExportadas.csv";
 
+            using (var fluxoDoArquivo = new FileStream(caminhoNovoArquivo, FileMode.Create))
+            {
+                var contaComoString = "456,23455,2330.20,Gustavo Santos";
+                var encoding = Encoding.UTF8;
+                var bytes = encoding.GetBytes(contaComoString);
+
+                fluxoDoArquivo.Write(bytes, 0, bytes.Length);
+            }
+
+        }
+
+        static void CriarArquivoComWriter()
+        {
+            var caminhoNovoArquivo = "contasExportadas.csv";
+            using (var fluxoDeArquivo = new FileStream(caminhoNovoArquivo, FileMode.Create))
+            using (var escritor = new StreamWriter(fluxoDeArquivo))
+            {
+                escritor.Write("123,1234,2000.21,Jose Santos");
+            }
+            
         }
     }
 }
--- ByteBankImportacaoExportacao/Program.cs
+++ ByteBankImportacaoExportacao/Program.cs
@@ -1,56 +1,27 @@
 ﻿using ByteBankImportacaoExportacao.Modelos;
 using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ByteBankImportacaoExportacao
-{
-    partial class Program
-    {
-        static void Main(string[] args)
+using System.IO; // IO = Input e Output
+using System.Globalization;
+
+namespace ByteBankImportacaoExportacao 
+{ 
+    partial class Program 
+    { 
+        static void Main(string[] args) 
         {
-            var enderecoArquivo = "contas.txt";
-            using (var fluxoDeArquivo = new FileStream(enderecoArquivo, FileMode.Open))
-            using (var leitor = new StreamReader(fluxoDeArquivo))
-            { 
-                while (!leitor.EndOfStream)
-                {
-                    var linha = leitor.ReadLine();
-                    var contaCorrente = ConverterStringEmContaCorrente(linha);
-                    var msg = $"##Conta Corrente## \nTitular: {contaCorrente.Titular.Nome} " +
-                        $"\nAg: {contaCorrente.Agencia}, Número: {contaCorrente.Numero} " +
-                        $"\nSaldo: {contaCorrente.Saldo}\n=-----------------=";
-                    Console.WriteLine(msg);
-                }
-            }                           
+            LeituraBinaria();
+
+
+            Console.WriteLine("Aplicação finalizada. . .");
+
 
             Console.ReadLine();
         }
-
-        static ContaCorrente ConverterStringEmContaCorrente(string linha)
-        {
-            string[] campos = linha.Split(',');
-
-            var agencia = campos[0];
-            var numero = campos[1];
-            var saldo = campos[2].Replace('.',',');
-            var nomeTitular = campos[3];
-
-            var agenciaInt = int.Parse(agencia);
-            var numeroInt = int.Parse(numero);
-            var saldoDouble = double.Parse(saldo);
-            var titular = new Cliente();
-            titular.Nome = nomeTitular;
-
-            var resultado = new ContaCorrente(agenciaInt, numeroInt);
-            resultado.Titular = titular;
-            resultado.Depositar(saldoDouble);
-
-            return resultado;
-        }
-       
     }
-}
+} 
+ 
